25414a59dbe628f12374f7283e3bb1d1b44578a9
heronet <sirat4757@gmail.com>
2023-01-14 02:16:11 +0600

    added create post

--- connect/Controllers/PostsController.cs
+++ connect/Controllers/PostsController.cs
@@ -1,6 +1,9 @@
+using System.Security.Claims;
 using connect.Data;
 using connect.Data.Dto;
 using connect.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +12,12 @@ namespace connect.Controllers;
 public class PostsController : BaseController
 {
     private readonly ApplicationDbContext _dbContext;
-    public PostsController(ApplicationDbContext dbContext) => _dbContext = dbContext;
+    private readonly UserManager<User> _userManager;
+    public PostsController(ApplicationDbContext dbContext, UserManager<User> userManager)
+    {
+        _dbContext = dbContext;
+        _userManager = userManager;
+    }
 
     [HttpGet]
     public async Task<ActionResult> GetPosts()
@@ -19,9 +27,48 @@ public class PostsController : BaseController
             .Include(p => p.Likes)
             .Include(p => p.Comments)
             .OrderByDescending(p => p.CreatedAt)
-            .Select(p => PostToDto(p))
             .ToListAsync();
-        return Ok(posts);
+        var postDtos = posts.Select(p => PostToDto(p));
+        return Ok(postDtos);
+    }
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult> AddPost(PostDto postDto)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null) return BadRequest("User does not exist");
+        var post = new Post
+        {
+            Text = postDto.Text,
+            User = user
+        };
+        _dbContext.Posts.Add(post);
+        if (await _dbContext.SaveChangesAsync() > 0)
+            return Ok(PostToDto(post));
+        return BadRequest("Creating Post Failed");
+    }
+
+    [Authorize]
+    [HttpDelete("delete/{postId}")]
+    public async Task<ActionResult> DeletePost(Guid postId)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null) return BadRequest("User does not exist");
+        var post = await _dbContext.Posts
+            .Include(p => p.Likes)
+            .Include(p => p.Comments)
+            .Include(p => p.Photos)
+            .FirstOrDefaultAsync(p => p.Id == postId);
+        if (post == null)
+            return BadRequest("Post does not exist");
+        if (post.UserId != userId)
+            return Unauthorized("You cannot delete this post");
+        _dbContext.Posts.Remove(post);
+        if (await _dbContext.SaveChangesAsync() > 0)
+            return Ok(new { Message = $"Post {post.Id} deleted" });
+        return BadRequest("Deleting Post Failed");
     }
     private PostDto PostToDto(Post post)
     {
@@ -33,7 +80,8 @@ public class PostsController : BaseController
             CreatedAt = post.CreatedAt,
             UserId = post.UserId,
             UserName = post.User.Name,
-            LikesCount = post.Likes.Count
+            LikesCount = post.Likes?.Count ?? 0,
+            CommentsCount = post.Comments?.Count ?? 0
         };
     }
 }
--- connect/Data/Dto/PostDto.cs
+++ connect/Data/Dto/PostDto.cs
@@ -6,6 +6,7 @@ public class PostDto
     public string Title { get; set; }
     public string Text { get; set; }
     public long LikesCount { get; set; }
+    public long CommentsCount { get; set; }
     public DateTime CreatedAt { get; set; }
     public string UserId { get; set; }
     public string UserName { get; set; }
